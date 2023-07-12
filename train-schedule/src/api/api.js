bd8f52c671934995725744b6f6aa1dac39ed64ff
Devanshi Joshi <137262039+developwithdev@users.noreply.github.com>
2023-07-12 13:44:04 +0530

    Update api.js

--- train-schedule/src/api/api.js
+++ train-schedule/src/api/api.js
@@ -6,8 +6,22 @@ const API = axios.create({
   baseURL: URL
 });
 
-API.interceptors.request.use((req) => {
-  const jwtToken = ''; // ADD THE AUTHORIZATION TOKEN HERE
+const getToken = async() =>{
+  let {data} = await axios.post("http://20.244.56.144/train/auth", {
+    companyName: "Hyderabad Trains",
+    ownerName: "Devanshi Joshi",
+    rollNo: "20BK1A6615",
+    ownerEmail: "developwithdev@gmail.com",
+    clientID: "10d1eaff-7173-42d2-a57c-61b358ed8db6",
+    clientSecret: "SllforGFtMPtByEX"
+  })
+  return data.access_token
+}
+
+// console.log(await getToken())
+
+API.interceptors.request.use(async(req) => {
+  const jwtToken = await getToken()
   req.headers.authorization = `Bearer ${jwtToken}`;
   return req;
 });
